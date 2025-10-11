aa32ee0e641599ab6eb82770c8384f99ce1165f8
dorcax <dorcasibrahim667@gmail.com>
2025-10-12 00:07:02 +0100

    add upload feature

--- src/module/upload/upload.service.ts
+++ src/module/upload/upload.service.ts
@@ -24,6 +24,58 @@ async upload(id:string ,file:Express.Multer.File,order:number,user:userEntity){
 
 }
 
+// get upload by id  
 
+async uploadIds(ids:string[]){
+    return await this.prisma.upload.findMany({
+        where:{
+            id:{in:ids}
+        },
+        include:{
+            attachments:true,
+            hotels:true
+        }
+    })}
+
+
+// download the url 
+async download(id:string){
+return await this.prisma.upload.findUnique({
+    where:{
+        id
+    },
+    select:{
+        url:true,name:true,type:true
+    }
+})
+}
 // delete upload from cloudinary
+
+async deleteUpload(ids:string[],user:userEntity){
+    const uploads =await this.prisma.upload.findMany({
+        where:{
+            id:{in:ids},
+            user:{
+                id:user.id
+            }
+        }
+    }) 
+    // delete from cloudinary 
+    for(let upload of uploads){
+        if(upload.publicId){
+            await cloudinary.uploader.destroy(upload.publicId)
+        }
+    }
+
+    // delete in db 
+  return   await this.prisma.upload.deleteMany({
+        where:{
+            id:{in:ids},
+            user:{
+                id:user.id
+            }
+        }
+    })
+
+}
 }
