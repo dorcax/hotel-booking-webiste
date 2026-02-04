eac3010a200021dcaf9273473f0209a5889ec874
dorcax <dorcasibrahim667@gmail.com>
2026-02-04 17:43:11 +0100

    fixed

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,62 +1,77 @@
-import { Body, Controller, Get, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { loginDto, registerDto, userEntity, verifyEmailDto } from './auth.types';
+import {
+  loginDto,
+  registerDto,
+  userEntity,
+  verifyEmailDto,
+} from './auth.types';
 import { GoogleAuthGuard } from './guard/GoogleAuthGuard';
 import { Auth, AuthUser } from './deocorator/auth.decorator';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-  @Post("signup")
-   createUser(@Body() dto:registerDto){
-    
-    return this.authService.register(dto)
-    
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
+  @Post('signup')
+  createUser(@Body() dto: registerDto) {
+    return this.authService.register(dto);
+  }
 
-    }
+  @Post('login')
+  loginUser(@Body() dto: loginDto, @Res({ passthrough: true }) res: Response) {
+    return this.authService.login(dto, res);
+  }
 
-    @Post("login")
-    loginUser(@Body() dto:loginDto,@Res({passthrough:true}) res:Response){
-      return this.authService.login(dto,res)
+  @Post('verify-otp')
+  verifyOtp(@Body() dto: verifyEmailDto) {
+    return this.authService.verifyEmail(dto);
+  }
 
-    }
+  @Auth()
+  @Get()
+  async authUser(@AuthUser() user: userEntity) {
+    return this.authService.authUser(user);
+  }
 
-    @Post("verify-otp")
-    verifyOtp(@Body() dto:verifyEmailDto){
-     return this.authService.verifyEmail(dto)
+  // google login
+  @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
+  async handleLogin() {
+    // return await this.authService.handleLogin()
+    return { message: 'google authentication' };
+  }
 
-    }
+  @Get('google/callback')
+  @UseGuards(GoogleAuthGuard)
+  async redirect(@Req() req, @Res() res) {
+    const user = req.user;
+    const payload = { sub: user.id, role: user.role };
+    const token = await this.jwtService.signAsync(payload);
 
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
 
-    
-    @Auth()
-    @Get()
-    async authUser(@AuthUser() user:userEntity){
-      return this.authService.authUser(user)
+    const frontendUrl = process.env.FRONTEND_URL;
 
-    } 
-
-
-    // google login 
-    @Get("google/login")
-    @UseGuards(GoogleAuthGuard)
-    async handleLogin(){
-      // return await this.authService.handleLogin()
-      return {message:"google authentication"}
-    }
-
-
-    @Get("google/callback")
-    @UseGuards(GoogleAuthGuard)
-
-    async redirect(@Req() req,@Res() res){
-       const user = req.user; // contains user info + token
-
-  // Option 1: Redirect with token in query (for SPA frontends)
-  return res.redirect(`http://localhost:5173?token=${user.token}`);
-      // return {message:"user redirect"}
-    }
-
-     
-    
+    // Option 1: Redirect with token in query (for SPA frontends)
+    return res.redirect(`${frontendUrl}/`);
+    // return {message:"user redirect"}
+  }
 }
--- src/module/auth/strategies/GoogleStrategy.ts
+++ src/module/auth/strategies/GoogleStrategy.ts
@@ -21,7 +21,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientID: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
       //   http://localhost:3000/auth/google/callback
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: `${process.env.FRONTEND_URL}`,
       scope: ['profile', 'email'],
       passReqToCallback: true,
     };
